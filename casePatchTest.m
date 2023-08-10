fdb9c5794b895243daa81c7becdd725b76f07e33
pedgarcia1 <pedgarcia@itba.edu.ar>
2023-08-10 19:12:47 -0300

    patchtest sin constraints
    si aplico p y p interferencia segun teorico zunchado da igual que nx. el problema son los constraints

--- casePatchTest.m
+++ casePatchTest.m
@@ -6,7 +6,7 @@ E = 200e3; nu = 0.3;
 pressureNormal = 100;
 a = 300; b = a + 69.1; c = b + 100.1; h = 500;
 interferencia = 0.7427; precond = 1e7;
-nElementsZ = 2; nElementsR = 2; distorsion = 0;
+nElementsZ = 20; nElementsR = 20; distorsion = 0;
 
 %% Preprocess
 
@@ -14,14 +14,8 @@ elementType='Q4';          %'CST' 'LST' 'Q4' 'Q8' 'Q9'
 problemType='Axisymmetric';       %'Stress' 'Strain' 'Axisymmetric'
 nDimensions=2;              % Problem dimension
 
-[elements1,nodes1,vertexNodes1,sideNodes]=quadrilateralDomainMeshGenerator_catedra(elementType,'Straight',b-a,h,nElementsR,nElementsZ,0,distorsion);
-nodes1(:,1) = nodes1(:,1) + a;
-[elements2,nodes2,vertexNodes2,sideNodes2] = quadrilateralDomainMeshGenerator(elementType,'Straight',c-b,h,nElementsR,nElementsZ,0,distorsion);
-vertexNodes2 = vertexNodes2 + size(elements1,1); 
-sideNodes2 = sideNodes2 + size(nodes1,1);
-nodes2(:,1) = nodes2(:,1) + b;
-elements = [elements1;elements2+size(nodes1,1)];
-nodes = [nodes1;nodes2];
+[elements,nodes,vertexNodes1,sideNodes]=quadrilateralDomainMeshGenerator_catedra(elementType,'Straight',b-a,h,nElementsR,nElementsZ,0,distorsion);
+nodes(:,1) = nodes(:,1) + a;
 
 % Mesh plot
 figure; meshPlot(elements,nodes,'b','No');
@@ -38,53 +32,35 @@ nConstraints = length(sideNodes(2,:));
 boundaryConditionsArray = false(nNodes,nDimensions);    % Boundary conditions array true=fixed
 boundaryConditionsArray(sideNodes(1,:),2)=true;
 boundaryConditionsArray(sideNodes(3,:),2)=true;
-boundaryConditionsArray(sideNodes2(1,:),2)=true;
-boundaryConditionsArray(sideNodes2(3,:),2)=true;
+
 
 % Load definition
 pointLoadsArray = zeros(nNodes,nDimensions);            % Point load nodal value for each direction
 Rside = sideNodes(2,:);
 Lside = sideNodes(4,:);
 pointLoadsArray = distributedLoad_3(elementType,Lside,pointLoadsArray,nodes,pressureNormal);
-% pointLoadsArray = distributedLoad_3(elementType,Rside,pointLoadsArray,nodesPositionArray,-pressureNormal);
+pointLoadsArray = distributedLoad_3(elementType,Rside,pointLoadsArray,nodes,-51.8);
 
 %% Solver
-
-% Constraints
-% sideNodes1(2,:) - sideNodes2(4,:) = intereferencia
-C = zeros(nConstraints,nTotalDof);
-rightSideDOF = convertNode2Dof(sideNodes(2,:),nDimensions);
-leftSideDOF = convertNode2Dof(sideNodes2(4,:),nDimensions);
-n = 1;
-for i = 1:2:length(rightSideDOF)
-    C(n,rightSideDOF(i)) = 1*precond;
-    C(n,leftSideDOF(i)) = -1*precond;
-    n = n + 1;
-end
 
 % Stiffness calculation and assembly
 [stiffnessMatrix]=assembleStiffnessMatrix(elementType,elements,nodes,constitutiveMatrix);
 
-K = [stiffnessMatrix C'
-    C zeros(nConstraints,nConstraints)];
-
 % Matrix reduction
 isFixed = reshape(boundaryConditionsArray',1,[])';
 isFree = ~isFixed;
-isFreeC = isFree; isFreeC((end+1):(end+nConstraints)) = true;
 
 
 % Loads Vector rearrangement
 loadsVector = reshape(pointLoadsArray',1,[])';
-loadsVector((end+1):(end+nConstraints)) = 1*interferencia*precond; % interferencia????
 
 
 % Equation solving
-displacementsReducedVector = K(isFreeC,isFreeC)\loadsVector(isFreeC);
+displacementsReducedVector = stiffnessMatrix(isFree,isFree)\loadsVector(isFree);
 
 % Reconstruction
 displacementsVector = zeros(nTotalDof,1);
-displacementsVector(isFree) = displacementsVector(isFree) + displacementsReducedVector(1:end-nConstraints);
+displacementsVector(isFree) = displacementsVector(isFree) + displacementsReducedVector;
 % lagrangeMultipliers = displacementsReducedVector(end-nConstraints+1:end);
 
 %% Postprocess
